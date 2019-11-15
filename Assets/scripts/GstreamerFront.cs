d942c1f46205c1f22acb650490582e1c14f09b9b
gaseunam <lyitong@berkeley.edu>
2019-11-15 13:34:41 -0800

    gstreamer on windows; done

--- Assets/scripts/GstreamerFront.cs
+++ Assets/scripts/GstreamerFront.cs
@@ -1,30 +1,41 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
 using System.Collections;
-using System;
-using System.IO;
-
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using OpenCvSharp;
 public class GstreamerFront : MonoBehaviour
 {
     // Start is called before the first frame update
     private Texture2D texture;
     public Material material;
-    FileStream fs;
+    public int flag;
+    Mat fra;
+    VideoCapture reader;
+    [DllImport("GStreamerReader.dll")] private static extern void GR_Init(int port1, int port2);
+    [DllImport("GStreamerReader.dll")] private static extern IntPtr GR_GetFrame(ref int width, ref int height, int flag);
+    [DllImport("GStreamerReader.dll")] private static extern void GR_Release();
     void Start()
     {
         texture = new Texture2D(1280, 720);
+        if (flag == 1)
+        {
+            GR_Init(5000, 5001);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        fs = new FileStream("b.jpg", FileMode.Open, FileAccess.Read);
-        fs.Seek(0, SeekOrigin.Begin);
-        byte[] bytes = new byte[fs.Length];
-        fs.Read(bytes, 0, (int)fs.Length);
-        texture.LoadImage(bytes);
+        int width = 0, height = 0;
+        IntPtr data = GR_GetFrame(ref width, ref height, flag);
+        fra = new Mat(height, width, MatType.CV_8UC3, data);
+        texture.LoadImage(fra.ToBytes());
         material.mainTexture = texture;
         material.color = Color.white;
+    }
+    private void OnDestroy()
+    {
+        GR_Release();
     }
 }
--- Assets/scripts/GstreamerRear.cs
+++ Assets/scripts/GstreamerRear.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
-using System.IO;
-
+using OpenCvSharp;
 public class GstreamerRear : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,22 +12,31 @@ public class GstreamerRear : MonoBehaviour
     private Texture2D textureright;
     public Material materialleft;
     public Material materialright;
-    FileStream fs;
+    public int flag;
+    Mat fra;
+    [DllImport("GStreamerReader.dll")] private static extern void GR_Init(int port1, int port2);
+    [DllImport("GStreamerReader.dll")] private static extern IntPtr GR_GetFrame(ref int width, ref int height, int flag);
+    [DllImport("GStreamerReader.dll")] private static extern void GR_Release();
     void Start()
     {
         texture = new Texture2D(1280, 720);
         textureleft = new Texture2D(720, 720);
         textureright = new Texture2D(720, 720);
+
+        if (flag == 1)
+        {
+            GR_Init(5000, 5001);
+            Debug.Log("GStreamer Ready");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        fs = new FileStream("a.jpg", FileMode.Open, FileAccess.Read);
-        fs.Seek(0, SeekOrigin.Begin);
-        byte[] bytes = new byte[fs.Length];
-        fs.Read(bytes, 0, (int)fs.Length);
-        texture.LoadImage(bytes);
+        int width = 0, height = 0;
+        IntPtr data = GR_GetFrame(ref width, ref height, flag);
+        fra = new Mat(height, width, MatType.CV_8UC3, data);
+        texture.LoadImage(fra.ToBytes());
         Color[] c = texture.GetPixels(0, 0, 720, 720);
         textureleft.SetPixels(c);
         textureleft.Apply();
@@ -38,7 +48,9 @@ public class GstreamerRear : MonoBehaviour
         textureright.Apply();
         materialright.mainTexture = textureright;
         materialright.color = Color.white;
-
-
+    }
+    private void OnDestroy()
+    {
+        GR_Release();
     }
 }
